f19ca5af033c017f6e2581db15ce5b8aabea1e99
todboy <hd_any@126.com>
2020-03-02 23:48:53 +0800

    [*]axios

--- 20-axios/01-axios/src/main.js
+++ 20-axios/01-axios/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
@@ -10,3 +11,28 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+// 默认get请求
+axios({
+  url: "http://123.207.32.32:8000/home/multidata"
+}).then(res => {
+  console.log(res)
+})
+
+// axios({
+//   url: "http://123.207.32.32:8000/home/multidata",
+//   method: 'get'
+// }).then(res => {
+//   console.log(res)
+// })
+
+// 传递参数
+axios({
+  url: "http://123.207.32.32:8000/home/data",
+  params: {
+    type: 'pop',
+    page: 1
+  }
+}).then(res => {
+  console.log(res)
+})

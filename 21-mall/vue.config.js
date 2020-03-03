3ff9226d032169552528d773dc2a24992fb3f68f
todboy <hd_any@126.com>
2020-03-03 22:11:08 +0800

    [*]mall

---
+++ 21-mall/vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        // 'content': 'components/content',
+        // 'common': 'components/common',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  }
+}
